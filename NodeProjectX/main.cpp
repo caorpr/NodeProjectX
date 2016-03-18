8fc43a6185a606f3903028c1dc81bfc5bb682b99
caorpr <cand3952@740s-pl-115.local>
2016-03-18 11:54:33 -0600

    Initial Commit

---
+++ NodeProjectX/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NodeProjectX
+//
+//  Created by Anderson, Clayton on 3/18/16.
+//  Copyright © 2016 Anderson, Clayton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
